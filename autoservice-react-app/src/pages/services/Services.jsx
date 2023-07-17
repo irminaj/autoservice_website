9347f46d77eee8d1dc57db0dd36ba71beaa124f1
irminaj <irminaj@github.com>
2023-07-17 12:04:38 +0300

    add CTA and footer sections to services page

--- autoservice-react-app/src/pages/services/Services.jsx
+++ autoservice-react-app/src/pages/services/Services.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { FooterHero } from "../../components/footer/footerHero/FooterHero";
+import { BottomNavbar } from "../../components/footer/bottomNavbar/BottomNavbar";
+import { CTA } from "../../components/cta/CTA";
 
 export const Services = () => {
   return (
     <div>
-      <h1>Services</h1>
+      <CTA />
+      <FooterHero />
+      <BottomNavbar />
     </div>
   );
 };
