4084e34d1f33893266f4c116060e41a83ff9dac5
irminaj <irminaj@github.com>
2023-07-19 20:21:29 +0300

    add CTA and Footer sections to about us page

--- autoservice-react-app/src/pages/about/About.jsx
+++ autoservice-react-app/src/pages/about/About.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { AboutHeader } from "../../components/aboutHeader/AboutHeader";
+import { CTA } from "../../components/cta/CTA";
+import { FooterHero } from "../../components/footer/footerHero/FooterHero";
+import { BottomNavbar } from "../../components/footer/bottomNavbar/BottomNavbar";
 
 export const About = () => {
   return (
     <div>
       <AboutHeader />
+      <CTA />
+      <FooterHero />
+      <BottomNavbar />
     </div>
   );
 };
